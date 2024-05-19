c0d588f0381464767d37016bd6dbc2f9b7d50f9d
amohdi <mohdiabdo47@.gmailcom>
2024-05-19 13:56:27 +0100

    still need to finish heredoc

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: amohdi <amohdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:06:01 by amohdi            #+#    #+#             */
-/*   Updated: 2024/05/19 01:34:34 by amohdi           ###   ########.fr       */
+/*   Updated: 2024/05/19 13:13:57 by amohdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,18 +77,14 @@ static t_bool is_delimiter(char *buffer, char *delimiter)
 {
     int i;
 
-    i = 0;
     if (!buffer || !delimiter)
         return FALSE;
-    while(buffer[i] && delimiter[i])
-    {  
-        if (buffer[i] != delimiter[i])
-            return FALSE;
+    i = 0;
+    while(buffer[i] && delimiter[i] && buffer[i] == delimiter[i])
         ++i;
-    }
-    if (buffer[i] == '\n' && !delimiter[i] && !buffer[i + 1])
-        return (TRUE);
-    return (FALSE);
+    if (delimiter[i] || buffer[i])
+        return (FALSE);
+    return (TRUE);
 }
 
 static t_bool ft_check_for_quotes(char *og_delimiter)
@@ -100,8 +96,8 @@ static t_bool ft_check_for_quotes(char *og_delimiter)
     i = -1;
     while(og_delimiter[++i])
         if (is_quote(og_delimiter[++i]) == TRUE)
-            return TRUE;
-    return FALSE;
+            return FALSE;
+    return TRUE;
 }
 
 char *ft_get_value(t_exec *exec, char *key)
@@ -110,9 +106,9 @@ char *ft_get_value(t_exec *exec, char *key)
     if (!exec || (exec && !exec->env))
         return (free(key), NULL);
     env = exec->env;
-    while(exec)
+    while(env)
     {
-        if (ft_strcmp(env->key, key) == 0)
+        if (env->key && ft_strcmp(env->key, key) == 0)
             return (free(key), ft_strdup(env->value));
         env = env->next;
     }
@@ -126,32 +122,45 @@ static char *expand_in_heredoc(t_exec *exec, char *line)
     char *env_variable;
     char *expanded_line;
 
-    i = -1;
+    if (!line)
+        return NULL;
+    i = 0;
     len = 0;
     expanded_line = NULL;
     env_variable = NULL;
-    while(line [++i])
+    printf("line is before SEGV %s\n", line);
+    if (!line)
+        printf("oooops\n");
+    while(line[i])
     {
         len = 0;
         while (line[i + len] && line[i + len] != '$')
             len++;
-        if (line[i + len] && line[i + len + 1])
+        if (len && line[i + len] && line[i + len + 1])
             expanded_line = ft_strjoin(expanded_line, ft_substr(line, i, len));
         else if (line[i + len] && !line[i + len + 1])
             expanded_line = ft_strjoin(expanded_line, ft_substr(line, i, len + 1));
         i += len;
         if (line[i] == '$')
         {
-            while(line[i + len] && is_space(line[i + len]) == FALSE && is_quote(line[i + len]) == FALSE && line[i + len] == '$')
+            ++i;
+            len = 0;
+            while(line[i + len] && is_space(line[i + len]) == FALSE && line[i + len] != '$')
                 ++len;
-            if (len)
+            if (line[i + len] != '$')
             {
-                env_variable = ft_get_value(exec, ft_substr(line, i, len));
+                char *buff = ft_substr(line, i, len);
+                printf("buff is %s\n", buff);
+                env_variable = ft_get_value(exec, buff);
                 expanded_line = ft_strjoin(expanded_line, env_variable);
             }
+            else if (!len)
+
         }
+        // if (line[i])
+            // ++i;
     }
-    printf("line is after got expanded : %s\n", expanded_line);
+    printf("line is after got expanded : env_var %s || %s\n", env_variable, expanded_line);
     return expanded_line;
 }
 
@@ -161,17 +170,31 @@ static void here_doc_helper(t_exec *exec, int w_heredoc, char *og_delimiter, cha
     char    *buffer;
     t_bool  to_be_expanded;
     
+    line = NULL;
     buffer = NULL;
     to_be_expanded = ft_check_for_quotes(og_delimiter);
-    while(is_delimiter(buffer, delimiter) == FALSE)
+    while(is_delimiter(line, delimiter) == FALSE)
     {
-        free(buffer);
+        if (buffer)
+            free(buffer);
         line = readline(YELLOW"Heredoc>"RESET_COLORS);
-        if (to_be_expanded == TRUE && exec)
+        printf("line is |%s|     |%s|\n", line, delimiter);
+        if (!line)
+            {
+                printf("\n");
+                break;}
+        if (to_be_expanded == TRUE && exec && is_delimiter(line, delimiter) == FALSE)
+        {
             buffer = expand_in_heredoc(exec, line);
-        else
+            printf("got here and to_be_expanded is TRUE\n");
+        }
+        else if (is_delimiter(line, delimiter) == FALSE)
+        {
+            printf("line is %s buff %s", line, buffer);
             buffer = ft_strdup(line);
-        write(w_heredoc, buffer, ft_strlen(buffer));
+        }
+        if (buffer)
+            write(w_heredoc, buffer, ft_strlen(buffer));
     }
     close(w_heredoc);
 }
