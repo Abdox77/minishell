355542e1bb070122cfa6e8e35641672b8e61e007
aabou-ib <aabou-ib@c4r2p10.1337.ma>
2024-06-30 21:46:15 +0100

    FIXED WILDCARDS

--- src/wildcards.c
+++ src/wildcards.c
@@ -6,7 +6,7 @@
 /*   By: aabou-ib <aabou-ib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:56:11 by aabou-ib          #+#    #+#             */
-/*   Updated: 2024/06/30 18:22:28 by aabou-ib         ###   ########.fr       */
+/*   Updated: 2024/06/30 21:45:27 by aabou-ib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ size_t	count_matches(const char *pattern)
 		entry = readdir(dir);
 	}
 	closedir(dir);
+	if (count == 0)
+		return (1);
 	return (count);
 }
 
@@ -45,7 +47,7 @@ static size_t	calculate_total_size(char **args)
 	i = 0;
 	while (args[i] != NULL)
 	{
-		if ((strchr(args[i], '*')) && !has_space_or_tab(args[i]))
+		if ((ft_strchr(args[i], '*')) && !has_space_or_tab(args[i]))
 			total_size += count_matches(args[i]);
 		else
 			total_size++;
@@ -55,16 +57,21 @@ static size_t	calculate_total_size(char **args)
 }
 
 static void	add_matches_to_expanded_args(char **expanded_args, char **matches,
-		int *count)
+		int *count, char *arg)
 {
 	int	j;
 
 	j = 0;
-	while (matches[j] != NULL)
-	{
-		expanded_args[(*count)++] = strdup(matches[j]);
-		free(matches[j]);
-		j++;
+	if (!matches || !matches[0])
+		expanded_args[(*count)++] = ft_strdup(arg);
+	else
+	{	
+		while (matches[j] != NULL)
+		{
+			expanded_args[(*count)++] = ft_strdup(matches[j]);
+			free(matches[j]);
+			j++;
+		}
 	}
 	free(matches);
 }
@@ -82,8 +89,15 @@ static char	**expand_args_with_matches(char **args, char **expanded_args)
 		if ((strchr(args[i], '*')) && !has_space_or_tab(args[i]))
 		{
 			matches = list_matching_files(args[i]);
-			if (matches)
-				add_matches_to_expanded_args(expanded_args, matches, &count);
+			// if (!matches)
+			// {
+			// 	matches = malloc(2 * sizeof(char *));
+			// 	matches[0] = strdup(args[i]);
+			// 	matches[1] = NULL;
+				
+			// }
+			// // if (matches)
+				add_matches_to_expanded_args(expanded_args, matches, &count, args[i]);
 		}
 		else
 			expanded_args[count++] = strdup(args[i]);
@@ -104,18 +118,3 @@ char	**expand_wildcards(char **args)
 		return (perror("malloc failed"), NULL);
 	return (expand_args_with_matches(args, expanded_args));
 }
-
-// void	free_expanded_args(char **args)
-// {
-// 	int	i;
-
-// 	if (!args)
-// 		return ;
-// 	i = 0;
-// 	while (args[i] != NULL)
-// 	{
-// 		free(args[i]);
-// 		i++;
-// 	}
-// 	free(args);
-// }
